4cac60437d98758e2b732de9dde274e6152051e3
root <maxxytech99@gamil.com>
2024-04-20 17:05:36 -0700

    commit 3

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-#!/bin/bash
-
-# Check if the PYFILE environment variable is set
-if [ -z "$PYFILE" ]; then
-    echo "Error: PYFILE environment variable is not set."
-        exit 1
-        fi
-
-        # Check if the file exists
-        if [ ! -f "$PYFILE" ]; then
-            echo "Error: File $PYFILE not found."
-                exit 1
-                fi
-
-                # Run the Python script
-                python "0-run"
-                
+#!/usr/bin 
+print("hello python")                
